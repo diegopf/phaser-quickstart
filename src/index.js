870bdc44d9d9ad4f2d3185190b87285bd3831898
diegopf <diegopascual.f@gmail.com>
2018-03-11 11:27:20 +0100

    chore(project): make use of es6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
 
-var config = {
+const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
@@ -11,12 +11,12 @@ var config = {
     }
   },
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create
   }
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
 function preload() {
   this.load.setBaseURL("http://labs.phaser.io");
@@ -29,15 +29,15 @@ function preload() {
 function create() {
   this.add.image(400, 300, "sky");
 
-  var particles = this.add.particles("red");
+  const particles = this.add.particles("red");
 
-  var emitter = particles.createEmitter({
+  const emitter = particles.createEmitter({
     speed: 100,
     scale: { start: 1, end: 0 },
     blendMode: "ADD"
   });
 
-  var logo = this.physics.add.image(400, 100, "logo");
+  const logo = this.physics.add.image(400, 100, "logo");
 
   logo.setVelocity(100, 200);
   logo.setBounce(1, 1);
